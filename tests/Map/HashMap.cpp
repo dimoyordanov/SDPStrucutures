f431a7252ef38fd72000735d83c62c439eb48f5b
DimoY <75063792+DimoY@users.noreply.github.com>
2025-01-25 17:30:12 +0200

    remove hashmap test

--- tests/Map/HashMap.cpp
+++ tests/Map/HashMap.cpp
@@ -6,27 +6,27 @@
 
 #include "../../structures/Map/HashMap.hpp"
 
-// TEST(HashMap, HashMapBasic) {
-//     HashMap<std::string, int> list;
-//     list.put("hello", 1);
-//     EXPECT_EQ(list.contains("hello"), true);
-//     EXPECT_EQ(list.contains("Hello"), false);
-//     EXPECT_EQ(list.get("hello"),1);
-// }
+TEST(HashMap, HashMapBasic) {
+    HashMap<std::string, int> list;
+    list.put("hello", 1);
+    EXPECT_EQ(list.contains("hello"), true);
+    EXPECT_EQ(list.contains("Hello"), false);
+    EXPECT_EQ(list.get("hello"),1);
+}
 
-// TEST(HashMap, HashMapMany) {
-//     HashMap<std::string, int> list;
-//     list.put("hello", 1);
-//     list.put("hello1", 2);
-//     list.put("hello2", 3);
-//     list.put("hello3", 4);
-//     EXPECT_EQ(list.contains("hello"), true);
-//     EXPECT_EQ(list.contains("hello1"), true);
-//     EXPECT_EQ(list.contains("hello2"), true);
-//     EXPECT_EQ(list.contains("hello3"), true);
-//     EXPECT_EQ(list.contains("Hello"), false);
-//     EXPECT_EQ(list.get("hello"),1);
-//     EXPECT_EQ(list.get("hello1"),2);
-//     EXPECT_EQ(list.get("hello2"),3);
-//     EXPECT_EQ(list.get("hello3"),4);
-// }
+TEST(HashMap, HashMapMany) {
+    HashMap<std::string, int> list;
+    list.put("hello", 1);
+    list.put("hello1", 2);
+    list.put("hello2", 3);
+    list.put("hello3", 4);
+    EXPECT_EQ(list.contains("hello"), true);
+    EXPECT_EQ(list.contains("hello1"), true);
+    EXPECT_EQ(list.contains("hello2"), true);
+    EXPECT_EQ(list.contains("hello3"), true);
+    EXPECT_EQ(list.contains("Hello"), false);
+    EXPECT_EQ(list.get("hello"),1);
+    EXPECT_EQ(list.get("hello1"),2);
+    EXPECT_EQ(list.get("hello2"),3);
+    EXPECT_EQ(list.get("hello3"),4);
+}
